83ed834134c9d96c8af44bd615d8a92d30c378e3
hendersonyang <89329691+hendersonyang@users.noreply.github.com>
2021-12-29 10:53:38 -0800

    add redirects for root

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 module.exports = {
     async redirects() {
-      return [
-        {
-          source: '/:path',
-          destination: '/api/:path',
-          permanent: true,
-        },
-      ]
+        return [
+            {
+                source: '/:path',
+                destination: '/api/:path',
+                permanent: true,
+            }, {
+                source: '/',
+                destination: '/api',
+                permanent: true,
+            },
+        ]
     },
-  }
-  
+}
