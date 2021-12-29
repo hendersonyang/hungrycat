a4a11006eb92a49257b9def4d1baf183658ea472
hendersonyang <89329691+hendersonyang@users.noreply.github.com>
2021-12-29 10:30:13 -0800

    edit

---
+++ index.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+    res.status(200).send(`I AM VERY HUNGRY CAT! FEED ME FISH BY GOING TO /feed.`);
+}
--- pages/api/index.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).send(`I AM VERY HUNGRY CAT! FEED ME FISH BY GOING TO <a href="/feed">/feed</a>.`);
-}
