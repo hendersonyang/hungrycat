345a665cf140c35d5d02f04c73855eded9db79a9
hendersonyang <89329691+hendersonyang@users.noreply.github.com>
2021-12-30 10:30:41 -0800

    mongodb update

--- pages/api/feed.js
+++ pages/api/feed.js
@@ -1,4 +1,27 @@
 var count = 0;
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient(process.env.mongo, { useUnifiedTopology: true, useNewUrlParser: true }).connect(async (err, DB) => {
+    if (err) throw err;
+    console.log("Sucessfully conected to DB!")
+
+    const Database = DB.db('Main')
+    const stats = Database.collection('Stats')
+
+    let stat = await stats.findOne({
+        type: "global"
+    });
+    count = stat.count || 0
+    setInterval(() => {
+        stats.updateOne({
+            type: "global"
+        }, {
+            $set: {
+                count: count
+            }
+        })
+    }, 10000)
+})
 
 export default function handler(req, res) {
     count++
