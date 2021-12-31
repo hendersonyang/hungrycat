4a44b864b1dc30eb386fadb7dd495015d09e3bc3
hendersonyang <89329691+hendersonyang@users.noreply.github.com>
2021-12-30 20:53:15 -0800

    add resyncing

--- pages/api/feed.js
+++ pages/api/feed.js
@@ -3,18 +3,21 @@ const axios = require("axios")
 var count = 0;
 var oldCount = 0;
 
-axios.post(`https://counter.hendersonyang.repl.co/currentcount?key=${process.env.key}&v=b`).then(res => {
-    count = Number(res.data)
-    oldCount = Number(res.data)
-}).catch(error => {
-    count = 0
-    oldCount = 0
-})
+function sync() {
+    axios.post(`https://counter.hendersonyang.repl.co/currentcount?key=${process.env.key}&v=c`).then(res => {
+        count = Number(res.data)
+        oldCount = Number(res.data)
+    }).catch(error => {
+        count = 0
+        oldCount = 0
+    })
+}
 setInterval(() => {
     let tempcount = count
     if (tempcount - oldCount > 0) {
-        axios.post(`https://counter.hendersonyang.repl.co/countincrease?increase=${tempcount - oldCount}&key=${process.env.key}&v=b&count=${tempcount}`)
+        axios.post(`https://counter.hendersonyang.repl.co/countincrease?increase=${tempcount - oldCount}&key=${process.env.key}&v=c&count=${tempcount}`)
         oldCount = tempcount
+        sync()
     }
 }, 10000)
 
@@ -23,13 +26,14 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
     if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
+        res.status(200).end()
+        return
     }
     count++
     res.status(200).send(`You fed me 1 fish and I am still hungry. I have ate ${count} fishes so far.`);
 }
+sync()
